5ce0c86db80b659f1e9eafe239b275a7a26bbc81
Crystal (Cryst) <cryst@crystallized.sh>
2025-05-14 23:40:54 +1200

    :33

--- src/components/PhotoScrollableArea.tsx
+++ src/components/PhotoScrollableArea.tsx
@@ -25,7 +25,6 @@ const PhotoScrollableArea = () => {
             <br/>
             Hope you enjoy viewing my photos.
             <br/>
-            <br/>
             - Michael Bui
           </p>
         </div>
