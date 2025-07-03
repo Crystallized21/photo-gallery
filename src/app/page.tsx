6c81f80a6512d4ba9d595cb2e778b10ec0fb91a3
Michael Bui <cryst@crystallized.sh>
2025-07-04 00:43:14 +1200

    add foh component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import Image from "next/image";
+import FOHCarousel from "@/components/carouselv2/FOHCarousel";
 
 export default function Home() {
   return (
-    <div className="relative h-screen w-full">
-      <Image
-        alt="thing"
-        src={"https://images.unsplash.com/photo-1742853288141-b95880a1c5ea?q=80&w=3275&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
-        fill
-        priority
-        sizes="100vw"
-        style={{ objectFit: 'cover' }}
-      />
-    </div>
+    <FOHCarousel/>
   );
 }
